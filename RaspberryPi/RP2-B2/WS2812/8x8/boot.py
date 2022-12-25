f3ad6f0c04bf9a4959a53272a8cd336d698873fe
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2022-12-25 02:53:04 +0100

    BOOT SCREEN v0.1

--- RaspberryPi/RP2-B2/WS2812/8x8/boot.py
+++ RaspberryPi/RP2-B2/WS2812/8x8/boot.py
@@ -1,15 +1,22 @@
 from libs import Neopixel
 import utime
-import random
+import math
+import uasyncio
 
-matrix = Neopixel(64, 0, 15, "GRB", 8)
+matrix_size = 64
+matrix_w = 8
+matrix_h = matrix_size/matrix_w
+
+matrix = Neopixel(matrix_size, 0, 15, "GRB", matrix_w, 0)
 matrix.brightness(2)
 
 matrix.mClear()
 
+# CORNER TEST
+
 utime.sleep(1)
 
-screenTest = [(255,255,255),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(255,255,255),
+cornerTest = [(255,255,255),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(255,255,255),
               (0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),
               (0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),
               (0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),
@@ -18,4 +25,38 @@ screenTest = [(255,255,255),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(255
               (0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),
               (255,255,255),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(255,255,255)]
 
-matrix.mDrawData(screenTest)
+matrix.mDrawData(cornerTest)
+
+utime.sleep(5)
+
+matrix.mClear()
+
+utime.sleep(.5)
+
+# COLOR TEST
+
+i = 0
+
+loop = uasyncio.get_event_loop()
+
+colors = []
+for p in range(0, matrix_size):
+    colors.append(matrix.colorHSV(i*1024+p*1024,255,255))
+
+async def colorTest():
+     
+    matrix.mDrawData(colors)
+
+    temp = colors[0]
+    colors.pop(0)
+    colors.append(temp)
+
+    loop.create_task(colorTest())
+
+async def runColorTest():
+    loop.create_task(colorTest())
+    await uasyncio.sleep(5)
+    loop.stop()
+    matrix.mClear()
+
+uasyncio.run(runColorTest())
