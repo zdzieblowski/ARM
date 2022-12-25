1d75ddb86a7ce5c32bd586e8d3ad2fb52ef3a35e
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2022-12-25 05:00:49 +0100

    update

--- RaspberryPi/RP2-B2/WS2812/8x8/boot.py
+++ RaspberryPi/RP2-B2/WS2812/8x8/boot.py
@@ -3,41 +3,55 @@ import utime
 import math
 
 matrix_size = 64
-matrix_w = 8
+matrix_width = 8
+matrix_brightness = 16
 
-matrix = Neopixel(matrix_size, 0, 15, "GRB", matrix_w, 0)
-matrix.brightness(2)
+matrix = Neopixel(matrix_size, 0, 15, "GRB", matrix_width, 0)
+matrix.brightness(matrix_brightness)
 
-# 
+# Methods
 
-matrix.mClear()
-utime.sleep(.5)
+def run(func, func_pause = 0):
+    func(func_pause)
+#     utime.sleep(func_pause)
+    matrix.mClear()
+    utime.sleep(.5)
 
-# CORNER TEST
+# Algorithms
 
-matrix.mSetPixel(0,0,(255,255,255))
-matrix.mSetPixel(7,0,(255,255,255))
-matrix.mSetPixel(0,7,(255,255,255))
-matrix.mSetPixel(7,7,(255,255,255))
-matrix.show()
-utime.sleep(5)
+def cornerTest(pause):
+    matrix.mSetPixel(0,0,(255,255,255))
+    matrix.mSetPixel(7,0,(255,255,255))
+    matrix.mSetPixel(0,7,(255,255,255))
+    matrix.mSetPixel(7,7,(255,255,255))
+    matrix.show()
+    utime.sleep(pause)
 
-matrix.mClear()
-utime.sleep(.5)
+def colorTest(pause):
+    colors = []
+    
+    for p in range(0, matrix_size):
+        colors.append(matrix.colorHSV(p*1024,255,255))
 
-# COLOR TEST
+    temp_time = utime.time()
 
-colors = []
-for p in range(0, matrix_size):
-    colors.append(matrix.colorHSV(p*1024,255,255))
+    while temp_time > utime.time()-pause:
+        matrix.mDrawData(colors)
+     
+        temp = colors[0]
+        colors.pop(0)
+        colors.append(temp)
 
-temp_time = utime.time()
+def brightnessTest(pause):
+    grays = []
+    for p in range(0, matrix_size):
+        grays.append((p*4,p*4,p*4))
+        
+    matrix.mDrawData(grays)
+    utime.sleep(pause)
 
-while temp_time > utime.time()-5:
-    matrix.mDrawData(colors)
- 
-    temp = colors[0]
-    colors.pop(0)
-    colors.append(temp)
+# Run
 
-matrix.mClear()
+run(cornerTest, 2)
+run(colorTest, 2)
+run(brightnessTest, 2)
