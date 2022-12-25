d1f87f349c5699cebb3d90c23aec57745edf9694
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2022-12-25 04:16:14 +0100

    UPDATE

--- RaspberryPi/RP2-B2/WS2812/8x8/boot.py
+++ RaspberryPi/RP2-B2/WS2812/8x8/boot.py
@@ -5,46 +5,35 @@ import uasyncio
 
 matrix_size = 64
 matrix_w = 8
-matrix_h = matrix_size/matrix_w
 
 matrix = Neopixel(matrix_size, 0, 15, "GRB", matrix_w, 0)
 matrix.brightness(2)
 
 matrix.mClear()
+utime.sleep(.5)
 
 # CORNER TEST
 
-utime.sleep(1)
-
-cornerTest = [(255,255,255),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(255,255,255),
-              (0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),
-              (0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),
-              (0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),
-              (0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),
-              (0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),
-              (0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),
-              (255,255,255),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(255,255,255)]
-
-matrix.mDrawData(cornerTest)
-
+matrix.mSetPixel(0,0,(255,255,255))
+matrix.mSetPixel(7,0,(255,255,255))
+matrix.mSetPixel(0,7,(255,255,255))
+matrix.mSetPixel(7,7,(255,255,255))
+matrix.show()
 utime.sleep(5)
 
 matrix.mClear()
-
 utime.sleep(.5)
 
 # COLOR TEST
-
-i = 0
 
 loop = uasyncio.get_event_loop()
 
 colors = []
 for p in range(0, matrix_size):
-    colors.append(matrix.colorHSV(i*1024+p*1024,255,255))
+    colors.append(matrix.colorHSV(p*1024,255,255))
 
 async def colorTest():
-     
+    global colors
     matrix.mDrawData(colors)
 
     temp = colors[0]
@@ -60,3 +49,4 @@ async def runColorTest():
     matrix.mClear()
 
 uasyncio.run(runColorTest())
+
